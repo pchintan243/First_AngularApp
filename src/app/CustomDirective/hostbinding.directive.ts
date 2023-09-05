3f295e9e97e1c543eb873a1830cc170d2aa2a8f8
chintan <patelchintan843@gmail.com>
2023-09-05 13:47:14 +0530

    binding directive properties

--- src/app/CustomDirective/hostbinding.directive.ts
+++ src/app/CustomDirective/hostbinding.directive.ts
@@ -1,25 +1,33 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHostbinding]'
 })
-export class HostbindingDirective {
+export class HostbindingDirective implements OnInit {
 
   constructor(private element: ElementRef, private renderer: Renderer2) { }
 
+  @Input() defaultColor: string = 'transperent';
+  @Input() highlightColor: string = 'pink';
+
   @HostBinding('style.backgroundColor')
-  background: string = 'grey';
+  background: string = this.defaultColor;
   @HostBinding('style.border')
   border: string = 'solid 2px';
 
+  // This takes value from app component take blue color.
+  ngOnInit() {
+    this.background = this.defaultColor;
+  }
+
   @HostListener('mouseenter')
   onmousehover() {
-    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'yellow');
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.highlightColor);
   }
 
   @HostListener('mouseleave')
   onmouseleave() {
-    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'grey');
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.defaultColor);
   }
 
 }
