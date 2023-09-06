819feab50b511fa51815162baefee11561271d6c
chintan <patelchintan843@gmail.com>
2023-09-06 16:38:33 +0530

    observable parameter intro

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -9,15 +9,26 @@ import { Observable } from 'rxjs';
 export class ObservableComponent implements OnInit {
 
   myObservable = new Observable((observer) => {
-    observer.next("1");
-    observer.next("2");
-    observer.next("3");
+    setTimeout(() => { observer.next("1"); }, 1000);
+    setTimeout(() => { observer.next("2"); }, 2000);
+    // Error method show the error and after error observer would not be execute even complete observer not execute.
+    // setTimeout(() => { observer.error(new Error('Please try again later..!!')) }, 3000);
+    setTimeout(() => { observer.next("4"); }, 4000);
+    // If no error occurs then it emits the complete method which logic is defined in subscribe method.
+    setTimeout(() => { observer.complete(); }, 5000);
+
+    // This method not execute because of complete method execute before this one.
+    setTimeout(() => { observer.next("6"); }, 6000);
   })
 
   ngOnInit(): void {
     // subscribe method takes 3 optional parameter which is next, error, complete.
     this.myObservable.subscribe((val) => {
       console.log(val);
+    }, (err) => {
+      alert(err.message)
+    }, () => {
+      alert('Completed..!!');
     })
   }
 }
